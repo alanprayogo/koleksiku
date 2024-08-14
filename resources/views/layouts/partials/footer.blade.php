3ca745d4f59297f9411c4a7727494e2568ea4865
alanprayogo <alanbp98@gmail.com>
2024-08-14 14:16:16 +0700

    initial commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted text-sm-left d-block d-sm-inline-block text-center">Copyright © 2023.
+            Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin
+                template</a> from BootstrapDash. All rights reserved.</span>
+        <span class="float-sm-right d-block mt-sm-0 float-none mt-1 text-center">Hand-crafted & made
+            with <i class="ti-heart text-danger ms-1"></i></span>
+    </div>
+</footer>
